1d4f6acad719f5832c546bb2ecb1801cc5aeeb98
SpamLark <51059253+SpamLark@users.noreply.github.com>
2019-08-23 10:44:13 +0100

    Update SORLCUR records on specific course and admit term.sql

--- Banner/SORLCUR records on specific course and admit term.sql
+++ Banner/SORLCUR records on specific course and admit term.sql
@@ -6,6 +6,6 @@ FROM
 WHERE
      1=1
      AND sorlcur_term_code_admit = '201909'
-     AND sorlcur_program LIKE 'BENGH-LN'
+     AND sorlcur_program IN ('MSC-NRS/NMH', 'LLBH-LLK')
      --AND sorlcur_styp_code = 'P'
      AND spriden_change_ind IS NULL
