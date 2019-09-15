236c91a9c9b1517e144c2b91788ee9f020a317f8
SpamLark <51059253+SpamLark@users.noreply.github.com>
2019-09-15 11:21:56 +0100

    Create Student missing sorlcur Student Type check.sql

---
+++ Banner/Student missing sorlcur Student Type check.sql
@@ -0,0 +1,30 @@
+-- By admit term
+SELECT
+    spriden_id, sorlcur.*
+FROM
+    sorlcur
+    JOIN spriden on sorlcur_pidm = spriden_pidm
+WHERE
+    1=1
+    AND sorlcur_term_code_admit = '201909'
+    AND sorlcur_styp_code IS NULL
+    AND sorlcur_current_cde = 'Y'
+    AND spriden_change_ind IS NULL
+;
+
+
+-- Any true record with an end date in the future
+SELECT
+    spriden_id, sorlcur.*
+FROM
+    sorlcur
+    JOIN spriden on sorlcur_pidm = spriden_pidm
+WHERE
+    1=1
+    --AND sorlcur_term_code_admit = '201909'
+    AND sorlcur_styp_code IS NULL
+    AND sorlcur_current_cde = 'Y'
+    AND spriden_change_ind IS NULL
+    AND sorlcur_lmod_code = 'LEARNER'
+    AND sorlcur_end_date > sysdate
+;
