5f9736952b933633f1388a4374a5b42d80956c2b
Sam Clarke <blindedmongoose@users.noreply.github.com>
2019-11-04 21:45:10 +0000

    Create Academic Advisers and Advisees by Program.sql

---
+++ Banner/Academic Advisers and Advisees by Program.sql
@@ -0,0 +1,47 @@
+/*
+
+This query returns all students on specified program(s) who have an expected completion date in the future,
+and lists their current adviser.
+
+*/
+
+
+SELECT
+    s1.spriden_id AS "Student_Number",
+    s1.spriden_last_name || ', ' || s1.spriden_first_name AS "Student_Name",
+    sorlcur_term_code_admit AS "Admit_Term",
+    sorlcur_program AS "Programme",
+    sorlcur_end_date AS "Expected_Completion_Date",
+    t1.sgradvr_advr_code AS "Advisor_Code",
+    s2.spriden_last_name || ', ' || s2.spriden_first_name AS "Adviser_Name"
+
+FROM
+    sorlcur
+    JOIN spriden s1 ON sorlcur_pidm = s1.spriden_pidm
+    JOIN sgradvr t1 ON sorlcur_pidm = t1.sgradvr_pidm
+    JOIN spriden s2 ON sgradvr_advr_pidm = s2.spriden_pidm
+
+WHERE
+    1=1
+    
+    -- Spriden criteria
+    AND s1.spriden_change_ind IS NULL
+    AND s2.spriden_change_ind IS NULL
+    
+    -- Curriculum criteria
+    AND sorlcur_lmod_code = 'LEARNER'
+    AND sorlcur_term_code_end IS NULL
+    AND sorlcur_end_date > sysdate
+    AND sorlcur_program IN ('BSCH-PX') -- Enter programme codes here!
+
+    -- Max Advisor Record
+    AND t1.sgradvr_term_code_eff = (
+        SELECT MAX(t2.sgradvr_term_code_eff)
+        FROM sgradvr t2
+        WHERE t2.sgradvr_pidm = t1.sgradvr_pidm)
+    
+ORDER BY
+    s2.spriden_last_name || ', ' || s2.spriden_first_name,
+    s1.spriden_last_name || ', ' || s1.spriden_first_name
+    
+;
