1c5dcb811b52dc4e76709c555e451cca900aec92
SpamLark <samuelrclarke@gmail.com>
2020-01-08 19:49:43 +0000

    Updates

--- Banner/EXAM COMMITTEES - Missing Marksheets 01-JAN.sql
+++ Banner/EXAM COMMITTEES - Missing Marksheets 01-JAN.sql
@@ -13,6 +13,7 @@ SELECT
     ssbsect_ptrm_code,
     ssbsect_ptrm_start_date,
     ssbsect_ptrm_end_date,
+    sfrstcr_rsts_code,
     shrgcom_name, 
     shrgcom_description,
     shrgcom_weight, 
@@ -32,6 +33,7 @@ FROM
     JOIN ssbsect ON shrmrks_crn = ssbsect_crn AND shrmrks_term_code = ssbsect_term_code
     JOIN scbcrse c1 ON ssbsect_subj_code = c1.scbcrse_subj_code AND ssbsect_crse_numb = c1.scbcrse_crse_numb AND c1.scbcrse_eff_term = (SELECT MAX(c2.scbcrse_eff_term) FROM scbcrse c2 WHERE c2.scbcrse_subj_code = c1.scbcrse_subj_code AND c2.scbcrse_crse_numb = c1.scbcrse_crse_numb) -- Issue here with courses that have changed name over time. Would need to return the title that the section's term code falls within
     JOIN spriden s1 ON shrmrks_pidm = s1.spriden_pidm and s1.spriden_change_ind IS NULL
+    JOIN sfrstcr ON shrmrks_term_code = sfrstcr_term_code AND shrmrks_crn = sfrstcr_crn AND shrmrks_pidm = sfrstcr_pidm
     LEFT JOIN sirasgn ON sirasgn_term_code = ssbsect_term_code AND sirasgn_crn = ssbsect_crn AND sirasgn_primary_ind = 'Y'
     LEFT JOIN spriden s2 ON sirasgn_pidm = s2.spriden_pidm AND s2.spriden_change_ind IS NULL
 WHERE
@@ -73,7 +75,10 @@ WHERE
 
     -- Only include rows from the marks table where either the score or the grade is null
     AND (shrmrks_score IS NULL OR shrmrks_grde_code IS NULL)
-
+	
+    -- Only include students who are still registered on the module
+    AND sfrstcr_rsts_code IN ('RE','RW')
+    
     -- Limit to 'off-campus' campuses
     -- AND ssbsect_camp_code NOT IN ('OBO', 'OBS')
 
