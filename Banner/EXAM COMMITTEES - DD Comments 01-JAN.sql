de2fe27c38aec0d299ca22df7d744eebe91aa764
SpamLark <samuelrclarke@gmail.com>
2020-01-08 20:06:43 +0000

    Updates

---
+++ Banner/EXAM COMMITTEES - DD Comments 01-JAN.sql
@@ -0,0 +1,84 @@
+SELECT 
+    shrmrks_term_code, 
+    shrmrks_crn, 
+    ssbsect_subj_code, 
+    ssbsect_crse_numb,
+    scbcrse_title,
+    ssbsect_camp_code,
+    ssbsect_ptrm_code,
+    ssbsect_ptrm_start_date,
+    ssbsect_ptrm_end_date,
+    sfrstcr_rsts_code,
+    shrgcom_name, 
+    shrgcom_description,
+    shrgcom_weight, 
+    s1.spriden_id,
+    s1.spriden_last_name || ', ' || s1.spriden_first_name AS "Student_Name",
+    shrmrks_score, 
+    shrmrks_percentage, 
+    shrmrks_grde_code, 
+    shrmrks_comments, 
+    shrmrks_completed_date, 
+    shrmrks_roll_date, 
+    shrmrks_data_origin,
+    s2.spriden_last_name || ', ' || s2.spriden_first_name AS "Module_Leader"
+
+FROM
+    shrmrks
+    JOIN shrgcom ON shrmrks_term_code = shrgcom_term_code AND shrmrks_crn = shrgcom_crn  AND shrmrks_gcom_id = shrgcom_id
+    JOIN ssbsect ON shrmrks_crn = ssbsect_crn AND shrmrks_term_code = ssbsect_term_code
+    JOIN scbcrse c1 ON ssbsect_subj_code = c1.scbcrse_subj_code AND ssbsect_crse_numb = c1.scbcrse_crse_numb AND c1.scbcrse_eff_term = (SELECT MAX(c2.scbcrse_eff_term) FROM scbcrse c2 WHERE c2.scbcrse_subj_code = c1.scbcrse_subj_code AND c2.scbcrse_crse_numb = c1.scbcrse_crse_numb) -- Issue here with courses that have changed name over time. Would need to return the title that the section's term code falls within
+    JOIN spriden s1 ON shrmrks_pidm = s1.spriden_pidm and s1.spriden_change_ind IS NULL
+    JOIN sfrstcr ON shrmrks_term_code = sfrstcr_term_code AND shrmrks_crn = sfrstcr_crn AND shrmrks_pidm = sfrstcr_pidm
+    LEFT JOIN sirasgn ON sirasgn_term_code = ssbsect_term_code AND sirasgn_crn = ssbsect_crn AND sirasgn_primary_ind = 'Y'
+    LEFT JOIN spriden s2 ON sirasgn_pidm = s2.spriden_pidm AND s2.spriden_change_ind IS NULL
+    
+WHERE
+    1=1
+
+    -- Only return module runs that meet these criteria
+    AND shrmrks_crn IN (
+        SELECT
+            ssbsect_crn
+        FROM
+            ssbsect
+        WHERE
+            1=1
+
+            -- You can remove the NOT in the following section of the query to bring through UMP modules
+
+            AND ssbsect_subj_code||chr(1)||ssbsect_crse_numb NOT IN (
+                SELECT gorsdav_pk_parenttab
+                FROM gorsdav
+                WHERE gorsdav_table_name = 'SCBCRKY' AND gorsdav_attr_name = 'UMP' AND sys.ANYDATA.accessvarchar2(gorsdav_value) = 'Y'
+            )
+
+            AND CONCAT(ssbsect_subj_code, ssbsect_crse_numb) NOT IN (
+                SELECT CONCAT(scrattr_subj_code, scrattr_crse_numb)
+                FROM scrattr
+                WHERE scrattr_attr_code = 'L7DS'
+            )
+            AND (
+                (ssbsect_term_code = '201901' 
+                    AND ssbsect_ptrm_code IN ('S21', 'T21', 'T31', 'E10', 'E12', 'F10', 'G10', 'I8'))
+                OR
+                (ssbsect_term_code = '201906' 
+                    AND ssbsect_ptrm_code IN ('S31', 'T41', 'J5','J7', 'K5'))
+                OR
+                (ssbsect_term_code = '201909' 
+                    AND ssbsect_ptrm_code IN ('S1', 'T1', 'A2', 'A3', 'A4', 'B1', 'D1'))
+                )
+        )
+	
+    -- Only include students who are still registered on the module
+    AND sfrstcr_rsts_code IN ('RE','RW')
+    
+    -- Limit to 'off-campus' campuses
+    -- AND ssbsect_camp_code NOT IN ('OBO', 'OBS')
+
+    -- Only include rows with a comment of 'Deferred Disciplinary'
+    AND shrmrks_comments IN ('Deferred Disciplinary')
+
+ORDER BY
+    shrmrks_crn, shrgcom_name, shrgcom_description, s1.spriden_last_name, s1.spriden_id
+;
