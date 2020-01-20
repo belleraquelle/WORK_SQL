3576074dc996b7ac6c09674627f1bc96e713f53a
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-01-20 14:36:26 +0000

    Update EXAM COMMITTEES - UMP Exam Book Popsels.sql

--- Banner/EXAM COMMITTEES - UMP Exam Book Popsels.sql
+++ Banner/EXAM COMMITTEES - UMP Exam Book Popsels.sql
@@ -25,7 +25,7 @@ WHERE
 		    AND t1.sorlcur_cact_code = 'ACTIVE'
 			AND t1.sorlcur_term_code_end IS NULL
 			AND t1.sorlcur_lmod_code = 'LEARNER'
-			AND ump_1 = 'Y'
+			AND (ump_1 = 'Y' OR (sorlcur_program IN ('CHEU', 'DHEU') AND sorlcur_camp_code IN ('OBO','OBS')))
 			AND sorlcur_end_date BETWEEN '01-DEC-2019' AND '20-JAN-2020'
 			
 		)
@@ -44,51 +44,51 @@ WHERE
 				AND shrdgmr_pidm = sorlcur_pidm AND shrdgmr_seq_no = sorlcur_key_seqno AND sorlcur_lmod_code = 'OUTCOME'
 				AND shrdgmr_pidm = spriden_pidm AND spriden_change_ind IS NULL
 				AND sorlcur_curr_rule = sobcurr_curr_rule
-				AND ump_1 = 'Y'
+				AND (ump_1 = 'Y' OR (sorlcur_program IN ('CHEU', 'DHEU') AND sorlcur_camp_code IN ('OBO','OBS')))
 				AND shrdgmr_degs_code = 'PN'
 			)
 ;
 
 --PINK BOOK SELECTION
 SELECT DISTINCT
-	a1.sorlcur_pidm,
+	t1.sorlcur_pidm,
     spriden_id, 
-    a1.sorlcur_key_seqno,
-    a1.sorlcur_program
+    t1.sorlcur_key_seqno,
+    t1.sorlcur_program
 FROM
-	sorlcur a1,
+	sorlcur t1,
 	sobcurr_add,
-    sgbstdn t1,
-    sgrstsp v1,
+    sgbstdn t3,
+    sgrstsp t4,
     spriden
 WHERE
 	1=1
     AND sorlcur_pidm = spriden_pidm AND spriden_change_ind IS NULL
 	AND sorlcur_curr_rule = sobcurr_curr_rule
-    AND a1.sorlcur_pidm = t1.sgbstdn_pidm
-    AND a1.sorlcur_pidm = v1.sgrstsp_pidm AND a1.sorlcur_key_seqno = v1.sgrstsp_key_seqno
-	AND a1.sorlcur_term_code = (
+    AND t1.sorlcur_pidm = t3.sgbstdn_pidm
+    AND t1.sorlcur_pidm = t4.sgrstsp_pidm AND t1.sorlcur_key_seqno = t4.sgrstsp_key_seqno
+	AND t1.sorlcur_term_code = (
 		        SELECT MAX(a2.sorlcur_term_code)
 		        FROM sorlcur a2
-		        WHERE a2.sorlcur_pidm = a1.sorlcur_pidm AND a2.sorlcur_key_seqno = a1.sorlcur_key_seqno AND a2.sorlcur_lmod_code = 'LEARNER')
-	AND a1.sorlcur_current_cde = 'Y'
-	AND a1.sorlcur_cact_code = 'ACTIVE'
-	AND a1.sorlcur_lmod_code = 'LEARNER'
-    AND a1.sorlcur_term_code_end IS NULL
-    AND a1.sorlcur_term_code_admit <= '201909'
-    AND t1.sgbstdn_term_code_eff = (
+		        WHERE a2.sorlcur_pidm = t1.sorlcur_pidm AND a2.sorlcur_key_seqno = t1.sorlcur_key_seqno AND a2.sorlcur_lmod_code = 'LEARNER')
+	AND t1.sorlcur_current_cde = 'Y'
+	AND t1.sorlcur_cact_code = 'ACTIVE'
+	AND t1.sorlcur_lmod_code = 'LEARNER'
+    AND t1.sorlcur_term_code_end IS NULL
+    AND t1.sorlcur_term_code_admit <= '201909'
+    AND t3.sgbstdn_term_code_eff = (
 				SELECT MAX(t2.sgbstdn_term_code_eff)
 		        FROM sgbstdn t2
-		        WHERE t1.sgbstdn_pidm = t2.sgbstdn_pidm
+		        WHERE t3.sgbstdn_pidm = t2.sgbstdn_pidm
 			)
-    AND t1.sgbstdn_stst_code = 'AS'
-    AND v1.sgrstsp_term_code_eff = (
+    AND t3.sgbstdn_stst_code = 'AS'
+    AND t4.sgrstsp_term_code_eff = (
 		        SELECT MAX(v2.sgrstsp_term_code_eff)
 		        FROM sgrstsp v2
-		        WHERE v1.sgrstsp_pidm = v2.sgrstsp_pidm AND v1.sgrstsp_key_seqno = v2.sgrstsp_key_seqno
+		        WHERE t4.sgrstsp_pidm = v2.sgrstsp_pidm AND t4.sgrstsp_key_seqno = v2.sgrstsp_key_seqno
 		    )
-    AND v1.sgrstsp_stsp_code = 'AS'
-	AND ump_1 = 'Y'
+    AND t4.sgrstsp_stsp_code = 'AS'
+	AND (ump_1 = 'Y' OR (sorlcur_program IN ('CHEU', 'DHEU') AND sorlcur_camp_code IN ('OBO','OBS')))
 	--Students on a current X1 attribute or currently on a Foundation Year
 	AND (sorlcur_pidm||sorlcur_key_seqno IN (
 		SELECT
@@ -101,52 +101,52 @@ WHERE
 			AND s1.sgrsatt_pidm NOT IN
 				(SELECT s2.sgrsatt_pidm FROM sgrsatt s2 WHERE s1.sgrsatt_pidm = s2.sgrsatt_pidm AND s1.sgrsatt_stsp_key_sequence = s2.sgrsatt_stsp_key_sequence AND s2.sgrsatt_term_code_eff <= '201909' AND s2.sgrsatt_atts_code IN ('X2', 'X3','SW'))
 	)
-    OR a1.sorlcur_program IN ('FNDIP-FBE','FNDIP-FBU','FNDIP-FCO','FNDIP-FEG','FNDIP-FHU','FNDIP-FLL', 'FNDIP-LSF')
+    OR t1.sorlcur_program IN ('FNDIP-FBE','FNDIP-FBU','FNDIP-FCO','FNDIP-FEG','FNDIP-FHU','FNDIP-FLL', 'FNDIP-LSF')
     )
 ;
 
 --SILVER BOOK SELECTION
 SELECT DISTINCT
-	a1.sorlcur_pidm,
+	t1.sorlcur_pidm,
     spriden_id, 
-    a1.sorlcur_key_seqno,
-    a1.sorlcur_program
+    t1.sorlcur_key_seqno,
+    t1.sorlcur_program
 FROM
-	sorlcur a1,
+	sorlcur t1,
 	sobcurr_add,
-    sgbstdn t1,
-    sgrstsp v1,
+    sgbstdn t3,
+    sgrstsp t4,
     spriden
 WHERE
 	1=1
     AND sorlcur_pidm = spriden_pidm AND spriden_change_ind IS NULL
 	AND sorlcur_curr_rule = sobcurr_curr_rule
-    AND a1.sorlcur_pidm = t1.sgbstdn_pidm
-    AND a1.sorlcur_pidm = v1.sgrstsp_pidm AND a1.sorlcur_key_seqno = v1.sgrstsp_key_seqno
-	AND a1.sorlcur_term_code = (
+    AND t1.sorlcur_pidm = t3.sgbstdn_pidm
+    AND t1.sorlcur_pidm = t4.sgrstsp_pidm AND t1.sorlcur_key_seqno = t4.sgrstsp_key_seqno
+	AND t1.sorlcur_term_code = (
 		        SELECT MAX(a2.sorlcur_term_code)
 		        FROM sorlcur a2
-		        WHERE a2.sorlcur_pidm = a1.sorlcur_pidm AND a2.sorlcur_key_seqno = a1.sorlcur_key_seqno AND a2.sorlcur_lmod_code = 'LEARNER')
-	AND a1.sorlcur_current_cde = 'Y'
-	AND a1.sorlcur_cact_code = 'ACTIVE'
-	AND a1.sorlcur_lmod_code = 'LEARNER'
-    AND a1.sorlcur_term_code_end IS NULL
-    AND a1.sorlcur_term_code_admit <= '201909'
+		        WHERE a2.sorlcur_pidm = t1.sorlcur_pidm AND a2.sorlcur_key_seqno = t1.sorlcur_key_seqno AND a2.sorlcur_lmod_code = 'LEARNER')
+	AND t1.sorlcur_current_cde = 'Y'
+	AND t1.sorlcur_cact_code = 'ACTIVE'
+	AND t1.sorlcur_lmod_code = 'LEARNER'
+    AND t1.sorlcur_term_code_end IS NULL
+    AND t1.sorlcur_term_code_admit <= '201909'
     -- Only include students with an end date after the UCF
-    AND a1.sorlcur_end_date > '20-JAN-2020'
-    AND t1.sgbstdn_term_code_eff = (
-				SELECT MAX(t2.sgbstdn_term_code_eff)
-		        FROM sgbstdn t2
-		        WHERE t1.sgbstdn_pidm = t2.sgbstdn_pidm
+    AND t1.sorlcur_end_date > '20-JAN-2020'
+    AND t3.sgbstdn_term_code_eff = (
+				SELECT MAX(a3.sgbstdn_term_code_eff)
+		        FROM sgbstdn a3
+		        WHERE t3.sgbstdn_pidm = a3.sgbstdn_pidm
 			)
-    AND t1.sgbstdn_stst_code = 'AS'
-    AND v1.sgrstsp_term_code_eff = (
+    AND t3.sgbstdn_stst_code = 'AS'
+    AND t4.sgrstsp_term_code_eff = (
 		        SELECT MAX(v2.sgrstsp_term_code_eff)
 		        FROM sgrstsp v2
-		        WHERE v1.sgrstsp_pidm = v2.sgrstsp_pidm AND v1.sgrstsp_key_seqno = v2.sgrstsp_key_seqno
+		        WHERE t4.sgrstsp_pidm = v2.sgrstsp_pidm AND t4.sgrstsp_key_seqno = v2.sgrstsp_key_seqno
 		    )
-    AND v1.sgrstsp_stsp_code = 'AS'
-	AND ump_1 = 'Y'
+    AND t4.sgrstsp_stsp_code = 'AS'
+	AND (ump_1 = 'Y' OR (sorlcur_program IN ('CHEU', 'DHEU') AND sorlcur_camp_code IN ('OBO','OBS')))
 	--Exclude students on a current X1 attribute or currently on a Foundation Year
 	AND (sorlcur_pidm||sorlcur_key_seqno NOT IN (
 		SELECT
@@ -159,7 +159,7 @@ WHERE
 			AND s1.sgrsatt_pidm NOT IN
 				(SELECT s2.sgrsatt_pidm FROM sgrsatt s2 WHERE s1.sgrsatt_pidm = s2.sgrsatt_pidm AND s1.sgrsatt_stsp_key_sequence = s2.sgrsatt_stsp_key_sequence AND s2.sgrsatt_term_code_eff <= '201909' AND s2.sgrsatt_atts_code IN ('X2', 'X3','SW'))
 		)
-    	AND a1.sorlcur_program NOT IN ('FNDIP-FBE','FNDIP-FBU','FNDIP-FCO','FNDIP-FEG','FNDIP-FHU','FNDIP-FLL', 'FNDIP-LSF')
+    	AND t1.sorlcur_program NOT IN ('FNDIP-FBE','FNDIP-FBU','FNDIP-FCO','FNDIP-FEG','FNDIP-FHU','FNDIP-FLL', 'FNDIP-LSF')
     )
     -- Exclude students with pending awards
     AND sorlcur_pidm NOT IN (
@@ -175,7 +175,7 @@ WHERE
             AND shrdgmr_pidm = sorlcur_pidm AND shrdgmr_seq_no = sorlcur_key_seqno AND sorlcur_lmod_code = 'OUTCOME'
             AND shrdgmr_pidm = spriden_pidm AND spriden_change_ind IS NULL
             AND sorlcur_curr_rule = sobcurr_curr_rule
-            AND ump_1 = 'Y'
+            AND (ump_1 = 'Y' OR (sorlcur_program IN ('CHEU', 'DHEU') AND sorlcur_camp_code IN ('OBO','OBS')))
             AND shrdgmr_degs_code = 'PN'
     )
 ;
