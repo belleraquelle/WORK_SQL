d8bef6f8deb583bf77e94702c84793ec69e953df
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-01-22 09:47:53 +0000

    Create EXAM COMMITTEES - Banner Communication Manager.sql

---
+++ Banner/EXAM COMMITTEES - Banner Communication Manager.sql
@@ -0,0 +1,19 @@
+SELECT DISTINCT
+	shrapsp_pidm, 
+    shrapsp_astd_code_end_of_term,
+    shrapsp_prev_code
+FROM
+	shrapsp
+	JOIN sorlcur ON shrapsp_stsp_key_sequence = sorlcur_key_seqno AND shrapsp_pidm = sorlcur_pidm
+	JOIN sobcurr_add ON sorlcur_curr_rule = sobcurr_curr_rule
+WHERE
+    1=1
+	--AND shrapsp_astd_code_end_of_term = 'G3'
+	AND shrapsp_term_code = '201909'
+	AND shrapsp_activity_date >= '01-JAN-2020'
+    --AND shrapsp_pidm NOT IN (SELECT gorvisa_pidm FROM gorvisa WHERE gorvisa_vtyp_code = 'T4')
+	AND (ump_1 IS NULL AND sorlcur_program NOT IN ('CHEU','DHEU'))
+ORDER BY
+    shrapsp_astd_code_end_of_term,
+    shrapsp_prev_code
+;
