24f117ad0f74292d6a5646b213884370ad87fa75
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-03-06 10:23:42 +0000

    Update Module Sections with Module Leaders (including those with no ML).sql

--- Banner/Module Sections with Module Leaders (including those with no ML).sql
+++ Banner/Module Sections with Module Leaders (including those with no ML).sql
@@ -1,8 +1,10 @@
 SELECT 
     ssbsect_term_code,
+    ssbsect_ptrm_code,
     ssbsect_crn,
     ssbsect_subj_code,
     ssbsect_crse_numb,
+    c1.scbcrse_title,
     sirasgn_pidm,
     spriden_id,
     spriden_last_name,
@@ -11,6 +13,7 @@ SELECT
 FROM 
     sirasgn
     RIGHT JOIN ssbsect ON sirasgn_term_code = ssbsect_term_code AND sirasgn_crn = ssbsect_crn
+    RIGHT JOIN scbcrse c1 ON ssbsect_subj_code = c1.scbcrse_subj_code AND ssbsect_crse_numb = c1.scbcrse_crse_numb AND c1.scbcrse_eff_term = (SELECT MAX(c2.scbcrse_eff_term) FROM scbcrse c2 WHERE c2.scbcrse_subj_code = c1.scbcrse_subj_code AND c2.scbcrse_crse_numb = c1.scbcrse_crse_numb) -- Issue here with courses that have changed name over time. Would need to return the title that the section's term code falls within
     LEFT JOIN spriden ON sirasgn_pidm = spriden_pidm
 WHERE 
     1=1
