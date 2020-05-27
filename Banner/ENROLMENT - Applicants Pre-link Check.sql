d9bf2b0db08a29f61ec4938906a55477399c1f15
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-05-27 14:05:05 +0100

    Pre-link Check Query Update

--- Banner/ENROLMENT - Applicants Pre-link Check.sql
+++ Banner/ENROLMENT - Applicants Pre-link Check.sql
@@ -1,7 +1,73 @@
 SELECT DISTINCT
+	
+	CASE
+		WHEN 
+			SARADAP_RESD_CODE = '0' -- Invalid Residency Code
+			OR SARAATT_ATTS_CODE IS NULL -- No Student Attribute
+			OR sorlcur_curr_rule IS NULL -- No Curriculum Rule
+			OR sobcurr_program = 'DO NOT USE' -- Do Not Use Programme
+			OR saradap_program_1 != sorlcur_program -- Application and Curriculum Programme Don't Match
+			OR sorlcur_end_date IS NOT NULL -- Curriculum End Date Has Been Populated Early
+			OR goremal_email_address IS NULL -- Email Address is missing
+			OR sorlfos_lfos_rule IS NULL  --  No Major Rule
+		THEN null
+		ELSE 'Y'
+	END AS "Error_Free",
+	
+	CASE 
+		WHEN SARADAP_RESD_CODE = '0' -- Invalid Residency Code
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Invalid_Residency_Code",
+	
+	CASE 
+		WHEN SARAATT_ATTS_CODE IS NULL -- No Student Attribute
+		THEN 'Y'
+		ELSE NULL 
+	END AS "No_Student_Attribute",
+	
+	CASE 
+		WHEN sorlcur_curr_rule IS NULL -- No Curriculum Rule
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Curriculum_Rule_Field_Blank",
+	
+	CASE 
+		WHEN sorlfos_lfos_rule IS NULL  --  No Major Rule
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Major_Rule_Field_Blank",
+	
+	CASE 
+		WHEN sobcurr_program = 'DO NOT USE' -- Do Not Use Programme
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Do_Not_Use_programme",
+	
+	CASE 
+		WHEN saradap_program_1 != sorlcur_program -- Application and Curriculum Programme Don't Match
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Application_and_Curriculum_Mismatch",
+	
+	CASE 
+		WHEN sorlcur_end_date IS NOT NULL -- Curriculum End Date Has Been Populated Early
+		THEN 'Y'
+		ELSE NULL 
+	END AS "Curriculum_End_Date_Not_Null",
+	
+	CASE 
+		WHEN goremal_email_address IS NULL -- Email Address is missing
+		THEN 'Y'
+		ELSE NULL 
+	END AS "No_Email_Address",
+	
     SARADAP_PIDM AS "PIDM",
     SPRIDEN_ID AS "BANNER_ID",
+    SPRIDEN_LAST_NAME || ', ' || SPRIDEN_FIRST_NAME AS "STUDENT_NAME",
     SARADAP_APPL_NO AS "APPLICATION_NUMBER",
+    GOREMAL_EMAL_CODE AS "EMAIL_TYPE",
+    GOREMAL_EMAIL_ADDRESS AS "EMAIL_ADDRESS",
     SARADAP_TERM_CODE_ENTRY AS "ADMIT_TERM",
     SARADAP_STYP_CODE AS "MODE_OF_STUDY",
     SARADAP_RESD_CODE AS "RESIDENCY",
@@ -12,11 +78,12 @@ SELECT DISTINCT
     SARAATT_ATTS_CODE AS "STUDENT_ATTRIBUTE",
     SARCHRT_CHRT_CODE AS "COHORT_CODE",
     SORLCUR_PROGRAM AS "CURRICULUM_PROGRAMME_CODE",
+    SMRPRLE_PROGRAM_DESC AS "PROGRAMME_DESCRIPTION",
     SORLCUR_CURR_RULE AS "CURRICULUM_RULE",
     SORLCUR_START_DATE AS "CURRICULUM_START_DATE",
     SOBCURR_PROGRAM AS "SOBCURR_PROGRAMME",
     SORLFOS_MAJR_CODE AS "MAJOR",
-    SMRPRLE_PROGRAM_DESC AS "PROGRAMME_DESCRIPTION"
+    SORLFOS_LFOS_RULE AS "MAJOR_RULE"
 FROM
     SARADAP -- Application Table
     JOIN SPRIDEN ON (SARADAP_PIDM = SPRIDEN_PIDM)
@@ -27,6 +94,7 @@ FROM
     JOIN SMRPRLE ON SARADAP_PROGRAM_1 = SMRPRLE_PROGRAM -- Programme Table for description
     JOIN SORLFOS ON SORLCUR_PIDM = SORLFOS_PIDM AND SORLCUR_SEQNO = SORLFOS_LCUR_SEQNO -- SORLFOS for Major
     LEFT JOIN SOBCURR ON SORLCUR_CURR_RULE = SOBCURR_CURR_RULE -- Check that SORLCUR_CURR_RULE is correct
+    LEFT JOIN GOREMAL ON SARADAP_PIDM = GOREMAL_PIDM 
 WHERE
     1=1
     AND s1.sarappd_seq_no = (
@@ -35,9 +103,10 @@ WHERE
         WHERE s2.sarappd_pidm = s1.sarappd_pidm and s2.sarappd_appl_no = s1.sarappd_appl_no)
     AND SARAPPD_APDC_CODE = 'UT'
     AND SARADAP_APST_CODE != 'W'
-    AND SARADAP_TERM_CODE_ENTRY = '202001'
+    AND SARADAP_TERM_CODE_ENTRY = '202006'
     AND spriden_change_ind is null
     AND sorlcur_current_cde = 'Y'
+    AND ((goremal_emal_code = 'PERS' AND goremal_preferred_ind = 'Y') OR goremal_emal_code IS NULL)
     --AND SARADAP_RESD_CODE != '0'
     --AND saraatt_atts_code is not null
     --AND sorlcur_curr_rule is not null
@@ -48,3 +117,4 @@ WHERE
     --AND saradap_pidm in (SELECT GLBEXTR_KEY FROM GLBEXTR WHERE GLBEXTR_SELECTION = 'SRC_LINK')
 ORDER BY
       SPRIDEN_ID
+;
