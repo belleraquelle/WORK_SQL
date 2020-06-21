2a2b3149565632e5e49b8d4d1bc4a50d0957efe0
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-06-21 18:11:45 +0100

    Updates

---
+++ Banner/Student Record Wiki/EXAM COMMITTEES - Post-Exam Committee Processing - CP Credit Limit Check.sql
@@ -0,0 +1,210 @@
+/*
+ * 
+ * Too much L4 CP credit check.
+ * 
+ */
+
+SELECT
+	a1.spriden_id AS "Student_ID",
+	g1.sgrstsp_key_seqno AS "Study_Path",
+	b1.sorlcur_program AS "Programme_of_Study",
+	SUM(i1.shrtckg_credit_hours) AS "Total_L4_CP_Credit"
+	
+
+FROM
+	spriden a1 -- Person Record
+	JOIN sorlcur b1 ON a1.spriden_pidm = b1.sorlcur_pidm -- Curriculum Record
+	JOIN sgbstdn f1 ON a1.spriden_pidm = f1.sgbstdn_pidm -- Learner Record
+	JOIN sgrstsp g1 ON b1.sorlcur_pidm = g1.sgrstsp_pidm AND b1.sorlcur_key_seqno = sgrstsp_key_seqno -- Study Path Record
+	JOIN shrtckn h1 ON a1.spriden_pidm = h1.shrtckn_pidm AND g1.sgrstsp_key_seqno = shrtckn_stsp_key_sequence -- Academic History Records
+	JOIN shrtckg i1 ON h1.shrtckn_pidm = i1.shrtckg_pidm AND h1.shrtckn_term_code = i1.shrtckg_term_code AND h1.shrtckn_seq_no = i1.shrtckg_tckn_seq_no -- Grade Record
+	JOIN scrattr j1 ON h1.shrtckn_subj_code = j1.scrattr_subj_code AND h1.shrtckn_crse_numb = j1.scrattr_crse_numb -- Module Attributes
+	JOIN shrtckl k1 ON h1.shrtckn_pidm = k1.shrtckl_pidm AND h1.shrtckn_term_code = k1.shrtckl_term_code AND h1.shrtckn_seq_no = k1.shrtckl_tckn_seq_no -- Level Record for Modules in Academic History
+	--JOIN shrgrde l1 ON i1.shrtckg_grde_code_final = l1.shrgrde_code AND k1.shrtckl_levl_code = l1.shrgrde_levl_code -- Grade Details
+	
+WHERE
+	1=1
+	
+	-- Limit to students in specified POPSEL
+--	AND a1.spriden_pidm IN (
+--		SELECT glbextr_key 
+--		FROM glbextr 
+--		WHERE glbextr_selection = :popsel_name AND glbextr_user_id = :popsel_user
+--		)
+	
+	-- Select current SPRIDEN record
+	AND a1.spriden_change_ind IS NULL
+	
+	-- Select Maximum Current SORLCUR record
+	AND b1.sorlcur_lmod_code = 'LEARNER'
+	AND b1.sorlcur_current_cde = 'Y'
+	AND b1.sorlcur_cact_code = 'ACTIVE'
+	AND b1.sorlcur_term_code_end IS NULL
+	AND b1.sorlcur_term_code = (
+	
+		SELECT MAX(b2.sorlcur_term_code)
+		FROM sorlcur b2
+		WHERE
+			1=1
+			AND b1.sorlcur_pidm = b2.sorlcur_pidm
+			AND b1.sorlcur_key_seqno = b2.sorlcur_key_seqno
+			AND b2.sorlcur_lmod_code = 'LEARNER'
+			AND b2.sorlcur_current_cde = 'Y'
+			AND b2.sorlcur_cact_code = 'ACTIVE'
+			AND b2.sorlcur_term_code_end IS NULL
+	
+	)
+	
+	-- Select Maximum Learner Record
+	AND f1.sgbstdn_term_code_eff = ( 
+	
+		SELECT MAX(f2.sgbstdn_term_code_eff)
+		FROM sgbstdn f2
+		WHERE f1.sgbstdn_pidm = f2.sgbstdn_pidm
+	
+	)
+	
+	-- Select Maximum Study Path Record
+	AND g1.sgrstsp_term_code_eff = ( 
+	
+		SELECT MAX(g2.sgrstsp_term_code_eff)
+		FROM sgrstsp g2
+		WHERE g1.sgrstsp_pidm = g2.sgrstsp_pidm AND g1.sgrstsp_key_seqno = g2.sgrstsp_key_seqno
+	
+	)
+	
+	-- Select Maximum Grade Record
+	AND i1.shrtckg_seq_no = (
+	
+		SELECT MAX(i2.shrtckg_seq_no)
+		FROM shrtckg i2
+		WHERE i1.shrtckg_pidm = i2.shrtckg_pidm AND i1.shrtckg_term_code = i2.shrtckg_term_code AND i1.shrtckg_tckn_seq_no = i2.shrtckg_tckn_seq_no
+	
+	)
+	
+	-- Limit to active students
+	AND f1.sgbstdn_stst_code = 'AS'
+	
+	-- Limit to active study paths
+	AND g1.sgrstsp_stsp_code = 'AS'
+	
+	-- Limit to modules from the following levels
+	AND j1.scrattr_attr_code IN ('L4')
+	
+	AND i1.shrtckg_grde_code_final = 'CP'
+	
+	
+GROUP BY 
+	a1.spriden_id,
+	g1.sgrstsp_key_seqno,
+	b1.sorlcur_program
+	
+HAVING 
+	SUM(i1.shrtckg_credit_hours) > 30
+;
+
+
+/*
+ * 
+ * Too much L5 CP credit check.
+ * 
+ */
+
+SELECT
+	a1.spriden_id AS "Student_ID",
+	g1.sgrstsp_key_seqno AS "Study_Path",
+	b1.sorlcur_program AS "Programme_of_Study",
+	SUM(i1.shrtckg_credit_hours) AS "Total_L5_CP_Credit"
+	
+
+FROM
+	spriden a1 -- Person Record
+	JOIN sorlcur b1 ON a1.spriden_pidm = b1.sorlcur_pidm -- Curriculum Record
+	JOIN sgbstdn f1 ON a1.spriden_pidm = f1.sgbstdn_pidm -- Learner Record
+	JOIN sgrstsp g1 ON b1.sorlcur_pidm = g1.sgrstsp_pidm AND b1.sorlcur_key_seqno = sgrstsp_key_seqno -- Study Path Record
+	JOIN shrtckn h1 ON a1.spriden_pidm = h1.shrtckn_pidm AND g1.sgrstsp_key_seqno = shrtckn_stsp_key_sequence -- Academic History Records
+	JOIN shrtckg i1 ON h1.shrtckn_pidm = i1.shrtckg_pidm AND h1.shrtckn_term_code = i1.shrtckg_term_code AND h1.shrtckn_seq_no = i1.shrtckg_tckn_seq_no -- Grade Record
+	JOIN scrattr j1 ON h1.shrtckn_subj_code = j1.scrattr_subj_code AND h1.shrtckn_crse_numb = j1.scrattr_crse_numb -- Module Attributes
+	JOIN shrtckl k1 ON h1.shrtckn_pidm = k1.shrtckl_pidm AND h1.shrtckn_term_code = k1.shrtckl_term_code AND h1.shrtckn_seq_no = k1.shrtckl_tckn_seq_no -- Level Record for Modules in Academic History
+	--JOIN shrgrde l1 ON i1.shrtckg_grde_code_final = l1.shrgrde_code AND k1.shrtckl_levl_code = l1.shrgrde_levl_code -- Grade Details
+	
+WHERE
+	1=1
+	
+	-- Limit to students in specified POPSEL
+--	AND a1.spriden_pidm IN (
+--		SELECT glbextr_key 
+--		FROM glbextr 
+--		WHERE glbextr_selection = :popsel_name AND glbextr_user_id = :popsel_user
+--		)
+	
+	-- Select current SPRIDEN record
+	AND a1.spriden_change_ind IS NULL
+	
+	-- Select Maximum Current SORLCUR record
+	AND b1.sorlcur_lmod_code = 'LEARNER'
+	AND b1.sorlcur_current_cde = 'Y'
+	AND b1.sorlcur_cact_code = 'ACTIVE'
+	AND b1.sorlcur_term_code_end IS NULL
+	AND b1.sorlcur_term_code = (
+	
+		SELECT MAX(b2.sorlcur_term_code)
+		FROM sorlcur b2
+		WHERE
+			1=1
+			AND b1.sorlcur_pidm = b2.sorlcur_pidm
+			AND b1.sorlcur_key_seqno = b2.sorlcur_key_seqno
+			AND b2.sorlcur_lmod_code = 'LEARNER'
+			AND b2.sorlcur_current_cde = 'Y'
+			AND b2.sorlcur_cact_code = 'ACTIVE'
+			AND b2.sorlcur_term_code_end IS NULL
+	
+	)
+	
+	-- Select Maximum Learner Record
+	AND f1.sgbstdn_term_code_eff = ( 
+	
+		SELECT MAX(f2.sgbstdn_term_code_eff)
+		FROM sgbstdn f2
+		WHERE f1.sgbstdn_pidm = f2.sgbstdn_pidm
+	
+	)
+	
+	-- Select Maximum Study Path Record
+	AND g1.sgrstsp_term_code_eff = ( 
+	
+		SELECT MAX(g2.sgrstsp_term_code_eff)
+		FROM sgrstsp g2
+		WHERE g1.sgrstsp_pidm = g2.sgrstsp_pidm AND g1.sgrstsp_key_seqno = g2.sgrstsp_key_seqno
+	
+	)
+	
+	-- Select Maximum Grade Record
+	AND i1.shrtckg_seq_no = (
+	
+		SELECT MAX(i2.shrtckg_seq_no)
+		FROM shrtckg i2
+		WHERE i1.shrtckg_pidm = i2.shrtckg_pidm AND i1.shrtckg_term_code = i2.shrtckg_term_code AND i1.shrtckg_tckn_seq_no = i2.shrtckg_tckn_seq_no
+	
+	)
+	
+	-- Limit to active students
+	AND f1.sgbstdn_stst_code = 'AS'
+	
+	-- Limit to active study paths
+	AND g1.sgrstsp_stsp_code = 'AS'
+	
+	-- Limit to modules from the following levels
+	AND j1.scrattr_attr_code IN ('L5')
+	
+	AND i1.shrtckg_grde_code_final = 'CP'
+	
+	
+GROUP BY 
+	a1.spriden_id,
+	g1.sgrstsp_key_seqno,
+	b1.sorlcur_program
+	
+HAVING 
+	SUM(i1.shrtckg_credit_hours) > 15
+;
---
+++ Banner/Student Record Wiki/EXAM COMMITTEES - Post-Exam Committee Processing - PN Awards with Uncapped Resits.sql
@@ -0,0 +1,112 @@
+/*
+ * This report gives a list of all of the status PN and status AW awards against students in the specified popsel who have a 'current' Exceptional
+ * Circumstances comment
+ * 
+ */
+
+
+SELECT DISTINCT
+	a1.spriden_id AS "Student_ID",
+	a1.spriden_last_name || ', ' || a1.spriden_first_name AS "Student_Name",
+	b1.sorlcur_coll_code AS "Faculty",
+	b1.sorlcur_camp_code AS "Campus",
+	--b1.sorlcur_program AS "Programme_Code",
+	--j1.smrprle_program_desc AS "Programme_Description",
+	l1.sorlcur_program AS "Award_Programme_Code",
+	n1.smrprle_program_desc AS "Award_Programme_Description",
+	k1.shrdgmr_degc_code AS "Award_Code",
+	k1.shrdgmr_degs_code AS "Award_Status",
+	m1.shrdgih_honr_code AS "Classification",
+	shrmrks_crn,
+	shrmrks_comments,
+	ssbsect_subj_code, 
+	ssbsect_crse_numb,
+	ssbsect_ptrm_code
+	
+	
+FROM
+	spriden a1 -- Person Record
+	JOIN sorlcur b1 ON a1.spriden_pidm = b1.sorlcur_pidm -- Curriculum Record
+	JOIN sobcurr_add e1 ON b1.sorlcur_program = e1.sobcurr_program -- Course record (SOACURR)
+	JOIN sgbstdn f1 ON a1.spriden_pidm = f1.sgbstdn_pidm -- Learner Record
+	JOIN sgrstsp g1 ON b1.sorlcur_pidm = g1.sgrstsp_pidm AND b1.sorlcur_key_seqno = sgrstsp_key_seqno -- Study Path Record
+	JOIN smrprle j1 ON b1.sorlcur_program = j1.smrprle_program -- Programme Record for Title
+	LEFT JOIN shrdgmr k1 ON b1.sorlcur_pidm = k1.shrdgmr_pidm AND b1.sorlcur_key_seqno = k1.shrdgmr_stsp_key_sequence -- Award Record
+	LEFT JOIN sorlcur l1 ON k1.shrdgmr_pidm = l1.sorlcur_pidm AND k1.shrdgmr_seq_no = l1.sorlcur_key_seqno AND l1.sorlcur_lmod_code = 'OUTCOME' AND l1.sorlcur_current_cde = 'Y' -- Award Curriculum Record
+	LEFT JOIN shrdgih m1 ON k1.shrdgmr_pidm = m1.shrdgih_pidm AND k1.shrdgmr_seq_no = m1.shrdgih_dgmr_seq_no -- Classification Record
+	LEFT JOIN smrprle n1 ON l1.sorlcur_program = n1.smrprle_program -- Programme Record for Title
+	JOIN shrmrks ON a1.spriden_pidm = shrmrks_pidm
+	JOIN ssbsect ON shrmrks_crn = ssbsect_crn AND shrmrks_term_code = ssbsect_term_code
+	
+WHERE
+	1=1
+	
+	-- Select current SPRIDEN record
+	AND a1.spriden_change_ind IS NULL
+	
+	-- Select Maximum Current SORLCUR record
+	AND b1.sorlcur_lmod_code = 'LEARNER'
+	AND b1.sorlcur_current_cde = 'Y'
+	AND b1.sorlcur_cact_code = 'ACTIVE'
+	AND b1.sorlcur_term_code = (
+	
+		SELECT MAX(b2.sorlcur_term_code)
+		FROM sorlcur b2
+		WHERE
+			1=1
+			AND b1.sorlcur_pidm = b2.sorlcur_pidm
+			AND b1.sorlcur_key_seqno = b2.sorlcur_key_seqno
+			AND b2.sorlcur_lmod_code = 'LEARNER'
+			AND b2.sorlcur_current_cde = 'Y'
+			AND b2.sorlcur_cact_code = 'ACTIVE'
+	
+	)
+	
+	-- Select Maximum Learner Record
+	AND f1.sgbstdn_term_code_eff = ( 
+	
+		SELECT MAX(f2.sgbstdn_term_code_eff)
+		FROM sgbstdn f2
+		WHERE f1.sgbstdn_pidm = f2.sgbstdn_pidm
+	
+	)
+	
+	-- Select Maximum Study Path Record
+	AND g1.sgrstsp_term_code_eff = ( 
+	
+		SELECT MAX(g2.sgrstsp_term_code_eff)
+		FROM sgrstsp g2
+		WHERE g1.sgrstsp_pidm = g2.sgrstsp_pidm AND g1.sgrstsp_key_seqno = g2.sgrstsp_key_seqno
+	
+	)
+	
+	-- Limit to active students
+	AND f1.sgbstdn_stst_code = 'AS'
+	
+	-- Limit to active study paths
+	AND g1.sgrstsp_stsp_code = 'AS'
+
+
+	-- Limit to records in specified popsel
+	AND a1.spriden_pidm in (SELECT glbextr_key FROM glbextr WHERE glbextr_selection = :popsel_name AND glbextr_user_id = :popsel_user)
+	
+	-- Limit to PN or AW awards
+	AND k1.shrdgmr_degs_code IN ('PN')
+	
+	-- Limit to students with an exceptional circumstances comment
+	AND shrmrks_comments = 'Exceptional Circumstances' AND shrmrks_completed_date >= '01-APR-20'
+	
+	-- Limit to modules ending in S2
+	AND ssbsect_ptrm_code IN ('S2','S12')
+
+	
+ORDER BY 
+	b1.sorlcur_coll_code, 
+	l1.sorlcur_program,
+	b1.sorlcur_camp_code
+	
+;
+
+SELECT * FROM shrmrks JOIN spriden ON shrmrks_pidm = spriden_pidm AND spriden_id = '17060873';
+
+SELECT * FROM ssbsect;
