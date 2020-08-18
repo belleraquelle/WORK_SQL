c702d255eee7d5ca8c63d68b9ce2367c7d1263f3
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-08-18 11:15:13 +0100

    Updates

--- Banner/ENROLMENT - List of new students for finance.sql
+++ Banner/ENROLMENT - List of new students for finance.sql
@@ -1,7 +1,13 @@
 -- 'New' students who need to be on the finance list. Will include early links and foundation progressions
 
 SELECT DISTINCT
-	spriden_id, a1.*
+	spriden_id,
+	spriden_last_name,
+	spriden_first_name,
+	a1.sorlcur_program,
+	a1.sorlcur_styp_code,
+	a1.sorlcur_start_date,
+	a1.sorlcur_end_date
 FROM
 	sorlcur a1
 	JOIN spriden ON a1.sorlcur_pidm = spriden_pidm AND spriden_change_ind IS NULL
@@ -11,13 +17,8 @@ WHERE
 	AND a1.sorlcur_cact_code = 'ACTIVE'
 	AND a1.sorlcur_current_cde = 'Y'
 	AND a1.sorlcur_term_code_admit = '202009'
-	AND a1.sorlcur_pidm NOT IN ( 
+ORDER BY
+	a1.sorlcur_program,
+	a1.sorlcur_styp_code
 	
-		SELECT glbextr_key
-		FROM glbextr
-		WHERE glbextr_selection IN (  
-			'20200817_LINK'
-			--'20200824_LINK'
-		)
-	
-	);
+	;
--- Banner/MDUU - Unfinished Processes.sql
+++ Banner/MDUU - Unfinished Processes.sql
@@ -2,3 +2,5 @@ select gkrpptr_parent, gkrpptr_child, gkrpptr_owner, gkrpptr_started, gkrpptr_fi
 from gkrpptr
 where 1=1 
 and gkrpptr_started is not null and gkrpptr_finished IS null;
+
+SELECT * FROM missing_end_dates;
