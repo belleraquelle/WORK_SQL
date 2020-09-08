3e04f5a03a459fc6aeac99f3734e839703fc311e
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-09-08 16:15:19 +0100

    Update EXCEPTION REPORT - New Students, Multiple Study Paths.sql

--- Banner/EXCEPTION REPORT - New Students, Multiple Study Paths.sql
+++ Banner/EXCEPTION REPORT - New Students, Multiple Study Paths.sql
@@ -1,9 +1,11 @@
 SELECT DISTINCT
 	spriden_id,
+	spriden_last_name, 
+	spriden_first_name,
 	s1.sorlcur_term_code_admit,
-	s1.sorlcur_program,
-	s2.sorlcur_term_code_admit,
-	s2.sorlcur_program
+	s1.sorlcur_program
+	--s2.sorlcur_term_code_admit,
+	--s2.sorlcur_program
 FROM 
 	sorlcur s1
 	JOIN sorlcur s2 ON s1.sorlcur_pidm = s2.sorlcur_pidm
@@ -14,6 +16,8 @@ WHERE
 	AND s1.sorlcur_lmod_code = 'LEARNER'
 	AND s2.sorlcur_lmod_code = 'LEARNER'
 	AND s1.sorlcur_key_seqno != s2.sorlcur_key_seqno
-	AND s1.sorlcur_program = s2.sorlcur_program
+	AND s1.sorlcur_term_code_end IS NULL
+	AND s2.sorlcur_term_code_end IS NULL
+	--AND s1.sorlcur_program = s2.sorlcur_program
 ORDER BY 
 	spriden_id
