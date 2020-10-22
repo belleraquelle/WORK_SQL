a8340d5fc2537342ec71e046b68d63dd683965a7
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-10-22 07:57:53 +0100

    Updates

--- Banner/DATA TIDY NEW - 11 - Enrolment Status Beyond Expected Completion Term.sql
+++ Banner/DATA TIDY NEW - 11 - Enrolment Status Beyond Expected Completion Term.sql
@@ -10,24 +10,26 @@ SELECT
 	a1.sorlcur_key_seqno AS "Study_Path",
 	a1.sorlcur_end_date AS "Expected_Completion_Date",
 	stvterm_code AS "Expected_End_Term",
-	sfrensp_term_code AS "Enrolment_Term",
-	sfrensp_ests_code AS "Enrolment_Status",
-	CASE 
-		WHEN a1.sorlcur_pidm || a1.sorlcur_key_seqno IN ( 
-		
-			SELECT sfrstcr_pidm || sfrstcr_stsp_key_sequence
-			FROM sfrstcr
-			WHERE sfrstcr_term_code = sfrensp_term_code AND sfrstcr_rsts_code = 'FE'
-		
-		) THEN 'Y'
-	END AS "Fee_Module_Registered_In_Term"
-		
+	sfrensp_term_code AS "SP_Enrolment_Term",
+	sfrensp_ests_code AS "SP_Enrolment_Status",
+	sfbetrm_term_code AS "Learner_Enrolment_Term",
+	sfbetrm_ests_code AS "Learner_Enrolment_Status"
+--	CASE 
+--		WHEN a1.sorlcur_pidm || a1.sorlcur_key_seqno IN ( 
+--		
+--			SELECT sfrstcr_pidm || sfrstcr_stsp_key_sequence
+--			FROM sfrstcr
+--			WHERE sfrstcr_term_code = sfrensp_term_code AND sfrstcr_rsts_code = 'FE'
+--		
+--		) THEN 'Y'
+--	END AS "Fee_Module_Registered_In_Term",
 	
 FROM
 	sorlcur a1 
 	JOIN stvterm ON a1.sorlcur_end_date BETWEEN stvterm_start_date AND stvterm_end_date
 	JOIN sfrensp ON a1.sorlcur_pidm = sfrensp_pidm AND a1.sorlcur_key_seqno = sfrensp_key_seqno
 	JOIN spriden ON a1.sorlcur_pidm = spriden_pidm AND spriden_change_ind IS NULL
+	JOIN sfbetrm ON a1.sorlcur_pidm = sfbetrm_pidm AND sfrensp_term_code = sfbetrm_term_code
 	
 WHERE 
 
@@ -51,6 +53,11 @@ WHERE
 	
 		)
 	
-	-- Identify records where there is an EL status recorded against the study path in a term beyond the student's expected completion term
+	-- Identify records where there is a status recorded against the study path in a term beyond the student's completion term
 	AND sfrensp_term_code > stvterm_code
+	AND sfrensp_ests_code = 'EN'
+	--AND sfbetrm_ests_code != 'EL'
+
+ORDER BY 
+	sfrensp_term_code
 ;
---
+++ Banner/DATA TIDY NEW - 20 - Continuing Students Not Enrolled.sql
@@ -0,0 +1,108 @@
+/*
+ * This query identifies continuing students who have not completed enrolment and so need to be put on UTWD 
+ */
+
+SELECT 
+	spriden_id,
+	a1.*,
+	szrenrl.*
+FROM 
+	sorlcur a1
+	JOIN spriden ON a1.sorlcur_pidm = spriden_pidm AND spriden_change_ind IS NULL
+	LEFT JOIN szrenrl ON a1.sorlcur_pidm = szrenrl_pidm
+	JOIN sgrstsp t1 ON a1.sorlcur_pidm = t1.sgrstsp_pidm AND a1.sorlcur_key_seqno = t1.sgrstsp_key_seqno
+	JOIN sgbstdn_add t4 ON a1.sorlcur_pidm = t4.sgbstdn_pidm
+WHERE
+	1=1
+	
+	-- Exclude Test Students
+	AND (spriden_ntyp_code IS NULL OR spriden_ntyp_code != 'TEST')
+	
+	-- SORLCUR requirements  
+	AND a1.sorlcur_lmod_code = 'LEARNER'
+	AND a1.sorlcur_cact_code = 'ACTIVE'
+	AND a1.sorlcur_current_cde = 'Y'
+	AND a1.sorlcur_term_code = ( 
+		
+		SELECT MAX(a2.sorlcur_term_code)
+		FROM sorlcur a2
+		WHERE
+			1=1
+			AND a1.sorlcur_pidm = a2.sorlcur_pidm
+			AND a1.sorlcur_key_seqno = a2.sorlcur_key_seqno
+			AND a2.sorlcur_lmod_code = 'LEARNER'
+			AND a2.sorlcur_cact_code = 'ACTIVE'
+			AND a2.sorlcur_current_cde = 'Y'
+	
+		)
+		
+	-- Exclude new students based on admit term	
+	AND a1.sorlcur_term_code_admit != '202009'
+	
+	-- Limit to students with a completion date in the future that is beyond the PG dissertation deadline
+	AND a1.sorlcur_end_date > '30-SEP-2020'
+	
+	-- Current student status is Active
+    AND t4.sgbstdn_term_code_eff = (
+        SELECT MAX(e2.sgbstdn_term_code_eff)
+        FROM sgbstdn e2
+        WHERE t4.sgbstdn_pidm = e2.sgbstdn_pidm
+    )
+    AND sgbstdn_stst_code = 'AS'
+    
+    -- Current study path status is Active
+    AND t1.sgrstsp_term_code_eff = (
+        SELECT MAX(a2.sgrstsp_term_code_eff)
+        FROM sgrstsp a2
+        WHERE t1.sgrstsp_pidm = a2.sgrstsp_pidm AND t1.sgrstsp_key_seqno = a2.sgrstsp_key_seqno
+    )
+    AND t1.sgrstsp_stsp_code = 'AS'
+    
+    -- Exclude AIE students
+    AND a1.sorlcur_camp_code NOT IN ('AIE')
+    
+    -- Exclude Research students
+    AND a1.sorlcur_levl_code != 'RD'
+	
+	-- Exclude students who are 'EN' for the study path in 202009
+	AND a1.sorlcur_pidm || a1.sorlcur_key_seqno NOT IN (
+	
+		SELECT sfrensp_pidm || sfrensp_key_seqno
+		FROM sfrensp
+		WHERE
+			1=1
+			AND sfrensp_term_code = '202009'
+			AND sfrensp_ests_code IN ('EN', 'WD', 'NS', 'AT')
+	
+	)
+	
+	-- Exclude students who are 'EN' at the learner level in 202009
+	AND a1.sorlcur_pidm || a1.sorlcur_key_seqno NOT IN (
+	
+		SELECT sfbetrm_pidm
+		FROM sfbetrm
+		WHERE
+			1=1
+			AND sfbetrm_term_code = '202009'
+			AND sfbetrm_ests_code IN ('EN', 'WD', 'NS', 'AT')
+	
+	)
+	
+	-- Exclude students who have completed academic and financial enrolment
+	AND a1.sorlcur_pidm NOT IN ( 
+	
+		SELECT szrenrl_pidm
+		FROM szrenrl
+		WHERE
+			1=1
+			AND szrenrl_term_code = '202009'
+			AND szrenrl_academic_enrol_status = 'CO'
+			AND szrenrl_financial_enrol_status = 'CO'
+			AND szrenrl_overall_enrol_status = 'CO'
+	
+	)	
+	
+ORDER BY 
+	a1.sorlcur_camp_code,
+	a1.sorlcur_program
+;
