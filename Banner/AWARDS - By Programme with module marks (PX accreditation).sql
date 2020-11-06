0eb6a5bb9d1db42662b99e9220e7d6b4c9f7c944
SpamLark <51059253+SpamLark@users.noreply.github.com>
2020-11-06 10:15:41 +0000

    Updates

---
+++ Banner/AWARDS - By Programme with module marks (PX accreditation).sql
@@ -0,0 +1,48 @@
+SELECT 
+	spriden_id,
+	spriden_last_name,
+	spriden_first_name,
+	shrdgmr_pidm, 
+	--shrdgmr_degc_code,
+	--shrdgmr_degs_code,
+	--shrdgih_honr_code,
+	--shrdgmr_grad_date,
+	shrdgmr_program,
+	shrtckn_subj_code||shrtckn_crse_numb,
+	shrtckn_crse_title,
+	CASE 
+		WHEN shrcmrk_percentage IS NOT NULL THEN shrcmrk_percentage
+		ELSE to_number(shrtckn_course_comment)
+	END AS "Module_Mark",
+	shrdgcm_comment AS "Overall_Average"
+	
+FROM 
+	shrdgmr 
+	JOIN spriden ON shrdgmr_pidm = spriden_pidm AND spriden_change_ind IS NULL
+	JOIN shrdgih ON shrdgmr_pidm = shrdgih_pidm AND shrdgmr_seq_no = shrdgih_dgmr_seq_no
+	JOIN shrtckn ON shrdgmr_pidm = shrtckn_pidm AND shrdgmr_stsp_key_sequence = shrtckn_stsp_key_sequence
+	JOIN shrdgcm ON shrdgmr_pidm = shrdgcm_pidm AND shrdgmr_seq_no = shrdgcm_dgmr_seq_no
+	LEFT JOIN shrcmrk ON shrtckn_pidm = shrcmrk_pidm AND shrtckn_term_code = shrcmrk_term_code AND shrtckn_crn = shrcmrk_crn
+	
+WHERE
+	1=1
+	AND shrdgmr_program LIKE '%PX%'
+	AND shrdgmr_grad_date >= '01-JUN-20'
+	AND shrdgmr_degs_code = 'AW'
+	
+ORDER BY 	
+	shrdgmr_grad_date,
+	spriden_last_name,
+	spriden_first_name,
+	shrtckn_crse_numb,
+	shrtckn_subj_code || shrtckn_crse_numb
+;
+
+
+SELECT * FROM shrdgmr;
+
+SELECT * FROM shrdgcm;
+
+SELECT * FROM shrcmrk;
+
+SELECT * FROM shrtckn;
--- Banner/EXCEPTION REPORT - More Than 150 Credits In The Year.sql
+++ Banner/EXCEPTION REPORT - More Than 150 Credits In The Year.sql
@@ -12,34 +12,58 @@ FROM
     JOIN ssbsect ON s1.sfrstcr_crn = ssbsect_crn AND s1.sfrstcr_term_code = ssbsect_term_code
     JOIN sorlcur a1 ON s1.sfrstcr_pidm = a1.sorlcur_pidm AND a1.sorlcur_lmod_code = 'LEARNER'
     JOIN spriden ON s1.sfrstcr_pidm = spriden_pidm AND spriden_change_ind IS NULL
-    
+    JOIN sfrensp ON a1.sorlcur_pidm = sfrensp_pidm AND a1.sorlcur_key_seqno = sfrensp_key_seqno
+    JOIN sgrstsp t1 ON a1.sorlcur_pidm = t1.sgrstsp_pidm AND a1.sorlcur_key_seqno = t1.sgrstsp_key_seqno
+  	JOIN sgbstdn t4 ON a1.sorlcur_pidm = t4.sgbstdn_pidm
+	
 WHERE
     1=1
-    -- Only include registered modules in the count
-    AND s1.sfrstcr_rsts_code IN ('RE','RW')
-    
-    -- Limit to modules that end in the current AY
-    AND ssbsect_ptrm_end_date BETWEEN '01-SEP-19' AND '31-AUG-20'
-    
-    -- Pick out correct sorlcur records for current AY
+      -- Pick out correct sorlcur records for current AY
     AND a1.sorlcur_term_code = (
         SELECT MAX(a2.sorlcur_term_code) 
         FROM sorlcur a2 
-        WHERE a1.sorlcur_pidm = a2.sorlcur_pidm AND a1.sorlcur_key_seqno = a2.sorlcur_key_seqno AND a1.sorlcur_lmod_code = 'LEARNER'
+        WHERE a1.sorlcur_pidm = a2.sorlcur_pidm AND a1.sorlcur_key_seqno = a2.sorlcur_key_seqno AND a1.sorlcur_lmod_code = 'LEARNER' AND a2.sorlcur_cact_code = 'ACTIVE' AND a2.sorlcur_current_cde = 'Y'
         )    
-    AND (('202001' BETWEEN sorlcur_term_code AND sorlcur_term_code_end) OR sorlcur_term_code_end IS NULL)
-    AND sorlcur_end_date >= '01-MAY-20'
+    --AND (('202101' BETWEEN sorlcur_term_code AND sorlcur_term_code_end) OR sorlcur_term_code_end IS NULL)
+    AND sorlcur_end_date >= '01-MAY-21'
     AND sorlcur_cact_code = 'ACTIVE'
     AND sorlcur_current_cde = 'Y'
     
+    -- Current student status is Active
+    AND t4.sgbstdn_term_code_eff = (
+        SELECT MAX(e2.sgbstdn_term_code_eff)
+        FROM sgbstdn e2
+        WHERE t4.sgbstdn_pidm = e2.sgbstdn_pidm
+    )
+    AND sgbstdn_stst_code = 'AS'
+    
+    -- Current study path status is Active
+    AND t1.sgrstsp_term_code_eff = (
+        SELECT MAX(a2.sgrstsp_term_code_eff)
+        FROM sgrstsp a2
+        WHERE t1.sgrstsp_pidm = a2.sgrstsp_pidm AND t1.sgrstsp_key_seqno = a2.sgrstsp_key_seqno
+    )
+    AND t1.sgrstsp_stsp_code = 'AS'
+    
+    -- Student is enrolled for specified term
+    AND sfrensp_term_code = '202009'
+    AND sfrensp_ests_code = 'EN'
+    
+    
+    -- Only include registered modules in the count
+    AND s1.sfrstcr_rsts_code IN ('RE','RW', 'RC')
+    
+    -- Limit to modules that end in the current AY
+    AND ssbsect_ptrm_end_date BETWEEN '01-SEP-20' AND '31-AUG-21'
+    
     -- Limit to foundation / undergraduate students
-    AND sorlcur_levl_code IN ('FD', 'UG')
+    AND sorlcur_levl_code IN ('FD','UG')
     
     -- Exclude students on SW this AY
     AND s1.sfrstcr_pidm NOT IN (
         SELECT sgrsatt_pidm
         FROM sgrsatt
-        WHERE sgrsatt_term_code_eff = '201909' AND sgrsatt_atts_code = 'SW'
+        WHERE sgrsatt_term_code_eff = '202009' AND sgrsatt_atts_code = 'SW'
     )
     
     -- Exclude specific wonky courses
@@ -49,15 +73,17 @@ WHERE
     AND sorlcur_program NOT LIKE 'FDA%'
     
     -- Only include students enrolled in both S1 and S2
-    AND s1.sfrstcr_pidm IN (SELECT sfrensp_pidm FROM sfrensp WHERE sfrensp_term_code = 201909 AND sfrensp_ests_code = 'EN')
-    AND s1.sfrstcr_pidm IN (SELECT sfrensp_pidm FROM sfrensp WHERE sfrensp_term_code = 202001 AND sfrensp_ests_code = 'EN')
+    AND s1.sfrstcr_pidm IN (SELECT sfrensp_pidm FROM sfrensp WHERE sfrensp_term_code = '202009' AND sfrensp_ests_code = 'EN')
+    AND s1.sfrstcr_pidm IN (SELECT sfrensp_pidm FROM sfrensp WHERE sfrensp_term_code = '202101' AND sfrensp_ests_code = 'EN')
     
     -- Exclude students on exchange
     AND s1.sfrstcr_pidm NOT IN (
         SELECT s2.sfrstcr_pidm 
         FROM sfrstcr s2 JOIN ssbsect z1 ON s2.sfrstcr_crn = z1.ssbsect_crn AND s2.sfrstcr_term_code = z1.ssbsect_term_code 
-        WHERE s2.sfrstcr_rsts_code IN ('RE','RW') AND z1.ssbsect_subj_code = 'EXCH' AND ssbsect_term_code IN ('201909','202001')
+        WHERE s2.sfrstcr_rsts_code IN ('RE','RW') AND z1.ssbsect_subj_code = 'EXCH' AND ssbsect_term_code IN ('202009','202101')
     )
+    
+    
     
 GROUP BY
     spriden_id,
