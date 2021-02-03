8c0554ff5ca11f94fca42ca84ff35bf333f396f0
SpamLark <51059253+SpamLark@users.noreply.github.com>
2021-02-03 15:20:32 +0000

    Update EXAM COMMITTEES - ExCom Reports - Unresolved D Decisions.sql

--- Banner/Student Record Wiki/EXAM COMMITTEES - ExCom Reports - Unresolved D Decisions.sql
+++ Banner/Student Record Wiki/EXAM COMMITTEES - ExCom Reports - Unresolved D Decisions.sql
@@ -3,6 +3,9 @@
  * selections. 
  * 
  * It can be used to, for example, find unresolved D3s at the July committees.
+ * 
+ * Re-work this so that it returns any student who currently has a max record of 'D%' recorded against a term earlier than
+ * the one currently going to committee.
  */
 
 SELECT 
@@ -11,18 +14,24 @@ SELECT
 	spriden_first_name AS "First_Name",
 	b1.sorlcur_program AS "Course",
 	glbextr_selection AS "Population",
-	shrapsp.*
+	s1.shrapsp_term_code AS "Decision_Term",
+	s1.shrapsp_astd_code_end_of_term AS "Decision_Code"
 	
 FROM 
-	shrapsp
+	shrapsp s1
 	JOIN glbextr ON shrapsp_pidm = glbextr_key
 	JOIN spriden ON shrapsp_pidm = spriden_pidm AND spriden_change_ind IS NULL
 	JOIN sorlcur b1 ON shrapsp_pidm = b1.sorlcur_pidm AND shrapsp_stsp_key_sequence = b1.sorlcur_key_seqno
 WHERE
 	1=1
-	AND shrapsp_term_code = :term_code
-	AND shrapsp_astd_code_end_of_term = :decision_code
-	AND glbextr_selection IN ('202007-UG-STAGE1','202007-UG-STAGEII','202007_GOLD')
+	AND s1.shrapsp_term_code = (
+		SELECT MAX(s2.shrapsp_term_code)
+		FROM shrapsp s2
+		WHERE s1.shrapsp_pidm = s2.shrapsp_pidm AND s1.shrapsp_stsp_key_sequence = s2.shrapsp_stsp_key_sequence
+	)
+	AND glbextr_selection = :population
+	AND shrapsp_term_code != (SELECT gtvsdax_external_code FROM gtvsdax WHERE gtvsdax_internal_code_group = 'OBU' AND gtvsdax_internal_code = 'EXAM_TERM')
+	AND shrapsp_astd_code_end_of_term LIKE 'D%'
 	
 	-- Latest Learner SORLCUR
 	AND b1.sorlcur_lmod_code = 'LEARNER'
@@ -42,3 +51,5 @@ WHERE
 	
 	)
 ;
+
+SELECT * FROM gtvsdax;
