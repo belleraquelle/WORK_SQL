d174ce06dca9a1c403c731963af0d8332fb3197d
SpamLark <51059253+SpamLark@users.noreply.github.com>
2021-02-10 13:30:58 +0000

    Update DATA TIDY NEW - 00 - RX Holds.sql

--- Banner/DATA TIDY NEW - 00 - RX Holds.sql
+++ Banner/DATA TIDY NEW - 00 - RX Holds.sql
@@ -6,7 +6,8 @@ SELECT DISTINCT
 	spriden_id, 
 	spriden_last_name, 
 	spriden_first_name, 
-	sorlcur_term_code_admit, 
+	sorlcur_term_code_admit,
+	sorlcur_program,
 	sprhold.*
 FROM 
 	sprhold
@@ -20,4 +21,5 @@ WHERE
 	AND s1.sorlcur_term_code_admit = (
 		SELECT MAX(s2.sorlcur_term_code_admit) FROM sorlcur s2 WHERE s1.sorlcur_pidm = s2.sorlcur_pidm AND s2.sorlcur_lmod_code = 'LEARNER'
 		)
+	ORDER BY sorlcur_term_code_admit, sorlcur_program
 ;
