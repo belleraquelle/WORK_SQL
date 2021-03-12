5766cc9c5998c2bc37b4b9fab33f04bcae6990aa
SpamLark <51059253+SpamLark@users.noreply.github.com>
2021-03-12 15:28:16 +0000

    Create Graduation Tables.sql

---
+++ Banner/Graduation Tables.sql
@@ -0,0 +1,11 @@
+SELECT spriden_pidm FROM spriden WHERE spriden_id = '17051431';
+
+SELECT * FROM skbgcat WHERE skbgcat_pidm = '1322810';
+
+SELECT * FROM shbcatt WHERE shbcatt_pidm = '1322810'; -- Form = SHACATQ
+
+SELECT * FROM shbcrmy;
+
+SELECT * FROM skrgord WHERE skrgord_pidm = '1322810';
+
+SELECT * FROM skrgori WHERE skrgori_pidm = '1322810';
