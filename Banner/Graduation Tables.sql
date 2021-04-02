ecea80e701212569c2ec78250c26576257f32efe
SpamLark <51059253+SpamLark@users.noreply.github.com>
2021-04-02 07:05:59 +0100

    Additional graduation tables
    Included some additional details on specific graduation tables.

--- Banner/Graduation Tables.sql
+++ Banner/Graduation Tables.sql
@@ -1,11 +1,22 @@
 SELECT spriden_pidm FROM spriden WHERE spriden_id = '17051431';
 
-SELECT * FROM skbgcat WHERE skbgcat_pidm = '1322810';
+SELECT * FROM skbgcat; --WHERE skbgcat_pidm = '1322810'; -- Important attendance log
 
-SELECT * FROM shbcatt WHERE shbcatt_pidm = '1322810'; -- Form = SHACATQ
+--SELECT * FROM shbcatt WHERE shbcatt_pidm = '1322810'; -- Form = SHACATQ
 
 SELECT * FROM shbcrmy;
 
-SELECT * FROM skrgord WHERE skrgord_pidm = '1322810';
+SELECT * FROM skrgord WHERE skrgord_pidm = '1322810'; -- Important order form
 
 SELECT * FROM skrgori WHERE skrgori_pidm = '1322810';
+
+SELECT * FROM skbgccr; -- Seating table
+
+
+-- Are we expecting them to pay? 
+
+skbgcat
+shbcatt
+shbcrmy
+skrgord
+skrgori
